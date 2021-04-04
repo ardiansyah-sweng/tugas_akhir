c5dd38839ee1192000113fe2e7b43d2383428ca1
muhnashir <muhammadnashir9@gmail.com>
2021-04-04 21:39:26 +0700

    menambahkan project laravel

---
+++ database/migrations/2021_03_25_045340_create_dosen_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDosenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dosen', function (Blueprint $table) {
+            $table->id();
+            $table->string('nipy', 15);
+            $table->string('nidn', 15);
+            $table->string('jabfung', 2);
+            $table->string('avatar')->default('avatar.png');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dosen');
+    }
+}
